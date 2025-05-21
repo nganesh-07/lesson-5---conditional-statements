7177e824089cc105b65047a0f72c916962665bd5
nganesh <nganesh@iu.edu>
2025-05-20 19:32:47 -0700

    project 5

---
+++ project 5.py
@@ -0,0 +1,7 @@
+temperature = float(input("What's the temperature like outside?"))
+
+if temperature > 75:
+    print("Buy yourself a cold drink today.")
+
+else:
+    print("Save your money for an actual hot day.")
